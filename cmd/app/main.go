4b5b57569ea13d844cc2a9477d8eaa20d5167aa4
indoseiki <indoseiki.ism@gmail.com>
2024-12-23 11:29:42 +0700

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -30,7 +30,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     os.Getenv("CORS"),
 		AllowMethods:     "GET,POST,PUT,DELETE",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
